1a7870f9b7c75143ccc2c2d73e943d17f265554b
nooralruhaimani <nooralruhaimani97@gmail.com>
2025-08-20 21:03:49 -0700

    got updated

---
+++ script.js
@@ -0,0 +1,17 @@
+let name = prompt("Enter your name");
+
+let age = prompt("Enter your age");
+
+const city = prompt("Enter your city");
+
+
+console.log(
+    "%cName: " + name + age + city ,
+     "color: green;"
+    );
+
+alert(
+    "Hello " + name + "! you are welcome here. Your age is " + age + " and your city is " + city + "."
+ );
+
+
